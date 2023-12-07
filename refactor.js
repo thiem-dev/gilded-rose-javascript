50dadbd1dc273bd6e691e87269a717aa20807c10
Thiem Huynh <thiem.huynh0@gmail.com>
2023-12-07 16:53:47 -0500

    split regular and special item logic

--- refactor.js
+++ refactor.js
@@ -1,3 +1,5 @@
+import { specialItemsList } from "./specialItemList.js";
+
 export class ItemR {
     constructor(name, sellIn, quality) {
       this.name = name;
@@ -7,44 +9,57 @@ export class ItemR {
 }
 
 export class MarketItem extends ItemR{
+
     constructor(name, sellIn, quality){
         super(name, sellIn, quality)
     }
 
-    reduceQuality(){
-        this.quality -= 1;
+    updateQuality(){
+        if(specialItemsList.includes(this.name)){
+            this.updateSpecialItemQuality()
+        } else {
+            this.updateRegularItemQuality();
+        }
+
+    }
+
+    updateRegularItemQuality(){
+        console.log(this.name, 'is a regular item')
+        return;
+        if(this.sellIn > 0 && this.quality > 0){
+            this.quality -= 1;
+        } else {
+            this.quality -= 2;
+        }
+    }
+
+    updateSpecialItemQuality(){
+        console.log(this.name, 'is a special item')
     }
 }
 
 
 export let itemsR = [];
 
-itemsR.push(new ItemR("+5 Dexterity Vest", 10, 20));
-itemsR.push(new ItemR("Aged Brie", 2, 0));
-itemsR.push(new ItemR("Elixir of the Mongoose", 5, 7));
-itemsR.push(new ItemR("Sulfuras, Hand of Ragnaros", 0, 80));
-itemsR.push(new ItemR("Backstage passes to a TAFKAL80ETC concert", 15, 20));
-itemsR.push(new ItemR("Conjured Mana Cake", 3, 6));
+itemsR.push(new MarketItem("+5 Dexterity Vest", 10, 20));
+itemsR.push(new MarketItem("Aged Brie", 2, 0));
+itemsR.push(new MarketItem("Elixir of the Mongoose", 5, 7));
+itemsR.push(new MarketItem("Sulfuras, Hand of Ragnaros", 0, 80));
+itemsR.push(new MarketItem("Backstage passes to a TAFKAL80ETC concert", 15, 20));
+itemsR.push(new MarketItem("Conjured Mana Cake", 3, 6));
 itemsR.push(new MarketItem("Frosted Flakes", 5, 8))
+itemsR.push(new MarketItem("Lucky Charms", 0, 4))
 
 
-
-
-
-export const updateQuality2 = () => {
+export const nextDay = () => {
     console.log(itemsR)
     for(let item of itemsR){
-        const constructor = item.constructor.name
-
-
-        
-        if(constructor === 'MarketItem'){
-            item.reduceQuality()
-        }
+        //end of day
+        item.updateQuality()
 
     }
 
     console.log(itemsR)
 }
 
-updateQuality2();
+nextDay();
---
+++ specialItemList.js
@@ -0,0 +1,6 @@
+export const specialItemsList = [
+    "Aged Brie",
+    "Sulfuras, Hand of Ragnaros",
+    "Backstage passes to a TAFKAL80ETC concert",
+    "Conjured Mana Cake"
+]
