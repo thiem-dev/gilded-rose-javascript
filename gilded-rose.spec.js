64100dbd162d4f58dd92c60f272a6d5b5ffb4cb4
Thiem Huynh <thiem.huynh0@gmail.com>
2023-12-07 15:59:37 -0500

    test skeleton commented

--- gilded-rose.spec.js
+++ gilded-rose.spec.js
@@ -12,3 +12,30 @@ describe("updateQuality", () => {
     expect(testItem.sellIn).toBe(4);
   });
 });
+
+// test: sellin = 0 | quality degrade x2 
+
+// test: quality negative
+
+// test: aged brie increases in quality | sellin days decrease
+
+
+// test quality over 50 | except sulfuras
+
+
+// test: Sulfuras, Hand of Ragnaros quality states the same | non zero sellIn day
+
+// test: concert increase in quality 
+  // concert less than 10 days, quality + 2
+
+  // concert less than 5 days, quality + 3
+
+  // concert day 0 or negative, quality = 0?
+
+
+
+
+
+//test:  conjured item degrade x2
+
+
